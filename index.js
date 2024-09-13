e86bf16e787ab071ce483fb739ab95d624bd8998
kingkrazee <kaydenhendrick04@gmail.com>
2024-09-13 15:42:15 +0200

    host

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let PORT = process.env.PORT || 5004
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:8080', 
+    origin: ['http://localhost:8080','m-and-n-hiring.web.app','*'], 
     credentials:true,
     headers: ['Content-type', 'Authorization']
 }))
